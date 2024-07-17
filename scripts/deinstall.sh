7a3117545238175abf0649ab749f944eb29dbe35
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-07-17 15:26:24 +0200

    Update deinstall.sh

--- scripts/deinstall.sh
+++ scripts/deinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl delete service nfs-server-service
+kubectl delete service nfs-server
 kubectl delete deployment nfs-server
 kubectl delete pvc nfs-root-pv-claim
 kubectl delete pv nfs-root-pv
